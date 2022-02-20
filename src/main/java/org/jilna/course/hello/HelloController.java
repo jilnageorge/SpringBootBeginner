ace5b099690157d433c61a05b24654cfe93fbbf6
Jilna George <jilnageorge@gmail.com>
2022-02-20 23:08:44 +0400

    spring boot sample project for beginners
    from java brains

---
+++ src/main/java/org/jilna/course/hello/HelloController.java
@@ -0,0 +1,14 @@
+package org.jilna.course.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String sayHI() {
+		return "Hi";
+	}
+
+}
---
+++ src/main/java/org/jilna/course/topic/TopicService.java
@@ -0,0 +1,45 @@
+package org.jilna.course.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	
+	List<Topic> topics = new ArrayList<> (Arrays.asList(
+			new Topic("Spring1", "Spring Framework 1", "Spring Framework Description 1"),
+			new Topic("Spring2", "Spring Framework 2", "Spring Framework Description 2"),
+			new Topic("Spring3", "Spring Framework 3", "Spring Framework Description 3")
+			));
+
+	public List<Topic> getAllTopics(){
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+		
+	}
+
+	public void updateTopic(String id, Topic topic) {
+		for(int i=0; i< topics.size(); i++) {
+			Topic t = topics.get(i);
+			if(t.getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+		
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(t -> t.getId().equals(id));
+	}
+}
